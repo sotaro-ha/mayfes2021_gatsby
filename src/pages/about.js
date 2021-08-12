feb79185188693b12de43859d740bc2e21aa170c
sotaro <alphacomplex.104729@gmail.com>
2021-08-12 12:44:56 +0900

    finished Gatsuby Tutorial part 2

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,13 +4,13 @@ import { Link } from 'gatsby'
 import Layout from '../components/layout'
 
 // Step 2: Define your component
-const IndexPage = () => {
+const AboutPage = () => {
   return (
-    <Layout pageTitle="Home Page">
-    <p>I'm making this by following the Gatsby Tutorial.</p>
-  </Layout>
+    <Layout pageTitle="About Me">
+      <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+    </Layout>
   )
 }
 
 // Step 3: Export your component
-export default IndexPage
+export default AboutPage
