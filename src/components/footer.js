6efe8557a05ebbb7d3c59e394fef38c142dc4258
sotaro-ha <alphacomplex.104729@gmail.com>
2021-09-18 00:39:47 +0900

    resolve static image

--- src/components/footer.js
+++ src/components/footer.js
@@ -9,10 +9,10 @@ const Footer = () => {
             <a href="/"><StaticImage src="../images/general/Logo_footer.svg" /></a>
             <p className={Styles.left}>Copyright (C) designing plus nine. All Rights Reserved.</p>
             <div className={Styles.footer_logo_box}>
-                <p className={Styles.right}><a href="https://note.com/dp9" target="_blank"><StaticImage src="../images/general/note.svg"
-                            width={45} height={45}/></a></p>
+                <p className={Styles.right} className={Styles.Logo}><a href="https://note.com/dp9" target="_blank"><StaticImage src="../images/general/note.svg"
+                           /></a></p>
                 <p className={Styles.right}><a href="https://twitter.com/DP9_Official?s=20" target="_blank"><StaticImage
-                            src="../images/general/twitter.svg" width={45} height={45}/></a></p>
+                            src="../images/general/twitter.svg" /></a></p>
             </div>
         </div>
     </footer>
