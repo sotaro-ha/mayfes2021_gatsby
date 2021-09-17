a50ad4b6dee02cb7eb3a69f099544861cb189a28
khsacc <hiroki@eqchem.s.u-tokyo.ac.jp>
2021-09-17 13:47:35 +0900

    code format

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,24 @@
 // Step 1: Import React
-import * as React from 'react'
-import { Link } from 'gatsby'
-import Layout from '../components/layout_index'
-import Hero from "../components/hero"
-import Title from "../components/title"
-import { Helmets } from '../components/helmet'
-
+import * as React from "react";
+import { Link } from "gatsby";
+import Layout from "../components/layout_index";
+import Hero from "../components/hero";
+import Title from "../components/title";
+import { Helmets } from "../components/helmet";
 
 // Step 2: Define your component
-const IndexPage = (props) => {    
+const IndexPage = (props) => {
   return (
     <>
-<head>
-<Helmets />
-</head>
-    <Layout >
-     <h1>{props.location.key}</h1>
-  </Layout>
-  </>
-  )
-}
+      <head>
+        <Helmets />
+      </head>
+      <Layout>
+        <h1>{props.location.key}</h1>
+      </Layout>
+    </>
+  );
+};
 
 // Step 3: Export your component
-export default IndexPage
+export default IndexPage;
